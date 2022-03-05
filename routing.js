8892b08962a82b1610bbfe9e5d0a7bf3ada59e33
Abdias Tellez <telles.abdias@gmail.com>
2022-03-05 02:59:34 -0800

    Add files via upload

---
+++ routing.js
@@ -0,0 +1,17 @@
+// Sending and receiving data in JSON format using POST method
+//
+// var xhr = new XMLHttpRequest();
+// var url = "https://hackermerced-api.herokuapp.com/";
+// xhr.open("POST", url, true);
+
+// xhr.setRequestHeader("Content-Type", "application/json");
+// xhr.onreadystatechange = function () {
+//     if (xhr.readyState === 4 && xhr.status === 200) {
+//         var json = JSON.parse(xhr.responseText);
+//         console.log(json.email + ", " + json.password);
+//     }
+// };
+// var data = JSON.stringify({"question_id": "1"});
+// xhr.send(data);
+
+console.log("helloworld");
